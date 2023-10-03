c40d63a0b30822cc50da4089143d7d1263574047
jazzbrix17 <90753921+jazzbrix17@users.noreply.github.com>
2023-10-03 09:00:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,17 +3,6 @@ import streamlit
 import snowflake.connector
 from urllib.error import URLerror
 
-# Don't run anything past while we're troubleshoot
-streamlit.stop()
-
-# Snowflake-Python Connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchone()
-streamlit.header("The fruit load contains:")
-streamlit.dataframe(my_data_rows)
-
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
@@ -59,3 +48,24 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding', add_my_fruit)
 
 my_cur("insert into fruit_load_list values ('from streamlit')");
+
+# Don't run anything past while we're troubleshoot
+streamlit.stop()
+
+# Snowflake-Python Connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_data_rows = my_cur.fetchone()
+streamlit.header("The fruit load contains:")
+# Snowflake related functions
+def get_fruit_load_list():
+    with cnx.cursor as my_cur():
+  my_cur.execute("select * from fruit_load_list")
+  return my_cur.fetchall()
+
+# Add a button to load the fruit
+if streamlit.button('Get fruit load list')
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list
+    streamlit.datafame(my_data_rows)
