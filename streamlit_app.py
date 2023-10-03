89c123db7450e2dc659900bbc9c7a3c30fd95113
jazzbrix17 <90753921+jazzbrix17@users.noreply.github.com>
2023-10-03 08:29:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,4 @@ streamlit.dataframe(my_data_rows)
 
 # Add textbox
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('Thanks for adding', add_my_fruit)
