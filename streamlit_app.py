45ebb10aaac77641c8ca51672c410bef30ad6c13
jazzbrix17 <90753921+jazzbrix17@users.noreply.github.com>
2023-10-03 09:06:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,11 +43,17 @@ else:
 
 except URLerror as e:
 
-# Add textbox
+# Add end user to add a fruit in the list
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+         my_cur("insert into fruit_load_list values ('from streamlit')")
+         return "Thanks for adding" + new_fruit
+      
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding', add_my_fruit)
-
-my_cur("insert into fruit_load_list values ('from streamlit')");
+if streamlit.button('Add a Fruit to the List'):
+     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+     back_from_function = insert_row_snowflake(fruit_choice)
+     streamlit.dataframe(back_from_function)
 
 # Don't run anything past while we're troubleshoot
 streamlit.stop()
